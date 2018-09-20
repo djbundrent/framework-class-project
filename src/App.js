9f85c4f75be65a7e2039d3354b48c650b8e38b7e
Devin Bundrent <devinbundrent@icloud.com>
2018-09-20 19:14:43 -0400

    Week 2 changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      people: [
+        { name: 'Jim', alive: true },
+        { name: 'Bob', alive: false },
+        { name: 'Tim', alive: true }
+      ],
+
+      hidden: true
+    }
+  }
+
+  toggleHidden = () => this.setState(state => ({hidden: !state.hidden}))
+
   render() {
     return (
       <div className="App">
-        <h1> Hello World </h1>
-        <p> This is my cool hello world project. </p>
-        <p className="red"> This text is RED </p>
+      <button onClick={ this.toggleHidden }>Toggle</button>
+
+        { this.state.hidden === false && (
+          <ul>
+            { this.state.people
+              .map (p => (
+                <li> {p.name} </li>
+              ))}
+          </ul>
+          )}
+
       </div>
     );
   }
 }
-
+/* */
 export default App;
