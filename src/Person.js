93d154915b671b9922ae94d21bd938eca0ad60a2
Devin Bundrent <devinbundrent@icloud.com>
2018-09-27 18:35:56 -0400

    Added Person Component

---
+++ src/Person.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Person extends Component{
+    constructor(){
+        super();
+    }
+
+    render() {
+        return (
+            const {name} = this.props.person;
+            <li> {name} </li>
+        );
+    }
+}
+
+export default Person
